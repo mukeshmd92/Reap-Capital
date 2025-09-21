c17bf6fe20b944442d672045082b85800a64eada
Bishnupada Maity <bishnupdm.1@gmail.com>
2025-09-21 22:28:57 +0530

    favicon

--- src/screens/Hp/sections/HeroSection/HeroSection.tsx
+++ src/screens/Hp/sections/HeroSection/HeroSection.tsx
@@ -73,7 +73,7 @@ export const HeroSection = ({ id }: { id: string }) => {
         <img
           src={correctarrow}
           alt="arrow background"
-          className="absolute    md:ml-110 mt-58 md:mt-10 w-[490px] h-[400px]  md:w-180 md:h-auto  opacity-100 z-0"
+          className="absolute md:ml-110 mt-58 md:mt-10 w-[490px] h-[400px]  md:w-180 md:h-auto  opacity-100 z-0"
         />
       </div>
       <div className="absolute right-[160px] md:right-[704px] mt-[730px] md:mt-[455px]">
@@ -95,7 +95,7 @@ export const HeroSection = ({ id }: { id: string }) => {
           transition={{ duration: 1, delay: 1 }}
         />
       </div>
-      <div className="  absolute mt-137 mr-[-390px]   md:mt-87 md:mr-[-1370px]">
+      <div className="  absolute mt-137 mr-[-390px]  md:mt-87 md:mr-[-1370px]">
         <motion.img
           src={sidearrow}
           className="mix-blend-color-dodge h-30 md:h-50 md:w-53"
@@ -104,7 +104,6 @@ export const HeroSection = ({ id }: { id: string }) => {
           transition={{ duration: 1, delay: 0.8 }}
         />
       </div>
-
       <div className="absolute mt-26 ml-[-330px] md:mt-25 md:ml-[-1180px]">
         <motion.img
           src={bluringimg}
@@ -114,7 +113,7 @@ export const HeroSection = ({ id }: { id: string }) => {
           transition={{ duration: 1.5, ease: "easeOut" }}
         />
       </div>
-      <div className="  absolute mt-93 md:mt-111 ml-[-394px] md:ml-[-1300px]">
+      <div className="absolute mt-93 md:mt-111 ml-[-394px] md:ml-[-1300px]">
         <motion.img
           src={Rocket}
           className="h-46 md:h-61"
